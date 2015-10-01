a01358cac6a2b0cf2d9aae0f6602af55758057a8
jerryzang <jerryc1214@gmail.com>
2015-10-02 07:34:02 +0800

    restore tick timer

--- Controls/Ctrl_Music.xaml.cs
+++ Controls/Ctrl_Music.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace Cycubeat.Controls
 {
@@ -10,14 +9,12 @@ namespace Cycubeat.Controls
 
         public void Play()
         {
-            CompositionTarget.Rendering += rendering;
             peekTimer.Enabled = true;
             Music.Play();
         }
 
         public void Stop()
         {
-            CompositionTarget.Rendering -= rendering;
             peekTimer.Enabled = false;
             Music.Stop();
         }
@@ -34,33 +31,14 @@ namespace Cycubeat.Controls
         private void loaded(object sender, System.Windows.RoutedEventArgs e)
         {
             Music.Source = new Uri("Music/Anthem.wav", UriKind.Relative);
-            //peekTimer.Tick += tick;
-        }
-
-        private void rendering(object sender, EventArgs e)
-        {
-            if (i < 70)
-            {
-                var musicTimingPoint = Convert.ToInt32(Music.Position.TotalMilliseconds) - 1800;
-                var isPeeking = false;
-                foreach (var timingPoint in Switcher.pageSwitcher.TimingPoints)
-                {
-                    if (musicTimingPoint - timingPoint >= -10 && musicTimingPoint - timingPoint < 10 && !isPeeking)
-                    {
-                        isPeeking = true;
-                        PeekEvent(Switcher.pageSwitcher.HitPoints[i]);
-                        i++;
-                        break;
-                    }
-                }
-            }
+            peekTimer.Tick += tick;
         }
 
         private void tick(object sender, EventArgs e)
         {
             if (i < 70)
             {
-                var musicTimingPoint = Convert.ToInt32(Music.Position.TotalMilliseconds);
+                var musicTimingPoint = Convert.ToInt32(Music.Position.TotalMilliseconds - 1800);
                 var isPeeking = false;
                 foreach (var timingPoint in Switcher.pageSwitcher.TimingPoints)
                 {
